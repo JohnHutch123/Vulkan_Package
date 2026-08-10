46a82e9fdabaae201008b238c514602151ad72b2
John Hutch <JohnHutchings17@gmail.com>
2026-08-11 06:32:51 +1000

    fixes

---
+++ Samples/TestVulkan/Win32/Debug/VulcanTest.d
@@ -0,0 +1,48 @@
+
+.\\Win32\Debug\VulcanTest.exe: VulcanTest.dpr \
+		VulkanPackage.inc \
+		D:\Vulkan_Package\Samples\TestVulkan\CommThread.dcu \
+		D:\Vulkan\src\PasVulkan.Image.BMP.dcu \
+		D:\Vulkan\src\PasVulkan.Image.TGA.dcu \
+		D:\Vulkan\src\PasVulkan.Image.Utils.dcu \
+		D:\Vulkan\externals\pasdblstrutils\src\PasDblStrUtils.dcu \
+		D:\Vulkan\externals\pasjson\src\PasJSON.dcu \
+		D:\Vulkan\src\PasVulkan.Image.PNG.dcu \
+		D:\Vulkan\src\PasVulkan.Image.QOI.dcu \
+		D:\Vulkan\src\PasVulkan.Streams.dcu \
+		D:\Vulkan\src\PasVulkan.Math.dcu \
+		D:\Vulkan\src\PasVulkan.Collections.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_PixelInfo.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_TextureInspector.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_Nodes.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Assert.dcu \
+		D:\Vulkan\src\PasVulkan.Framework.dcu \
+		D:\Vulkan\src\PasVulkan.NVIDIA.AfterMath.dcu \
+		D:\Vulkan\src\PasVulkan.HighResolutionTimer.dcu \
+		D:\Vulkan\src\PasVulkan.Compression.Deflate.dcu \
+		D:\Vulkan\src\PasVulkan.Image.JPEG.dcu \
+		D:\Vulkan\src\PasVulkan.XML.dcu \
+		D:\Vulkan\src\PasVulkan.Utils.dcu \
+		D:\Vulkan\src\PasVulkan.Types.dcu \
+		D:\Vulkan\src\PasVulkan.CPU.Info.dcu \
+		D:\Vulkan\externals\pucu\src\PUCU.dcu \
+		D:\Vulkan\externals\pasmp\src\PasMP.dcu \
+		D:\Vulkan\src\Vulkan.dcu \
+		c:\program files (x86)\embarcadero\studio\37.0\lib\Win32\debug\vcl.dcp \
+		c:\program files (x86)\embarcadero\studio\37.0\lib\Win32\debug\rtl.dcp \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_ShaderBuilder.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_VulkanAPI.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_PointerValidation.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_WindowVCL.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_Camera.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_DataStore.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_Lookups.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_Descriptors.dcu \
+		D:\Vulkan_Package\Samples\TestVulkan\Vulkan_Renderer_Single.dcu \
+		D:\Vulkan_Package\Samples\TestVulkan\Vulkan_Renderer_CommThread.dcu \
+		D:\Vulkan_Package\Samples\TestVulkan\Vulkan_LoaderStorer_glTF.dcu \
+		D:\Vulkan_Package\RunTime_Src\Vulkan_Components_Scene_Renderer.dcu \
+		D:\Vulkan_Package\Samples\TestVulkan\Vulkan_Render_Tests.dcu \
+		D:\Vulkan_Package\Samples\TestVulkan\VulkanTestMF.dcu \
+		c:\program files (x86)\embarcadero\studio\37.0\lib\Win32\debug\SysInit.dcu
